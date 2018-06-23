4b1393d7e499e5a2b90718572bb79ed814457892
Akshay Tiwari <axedestined2succeed@gmail.com>
2018-06-23 12:16:25 +0530

    adding more list topics

---
+++ 08_Lists/comprehension_test.py
@@ -0,0 +1,21 @@
+nums = [1,2,3,4,5,6,7]
+print(f"nums is {nums}")
+
+print(f"[x*10 for x in nums] is : {[x*10 for x in nums]}")
+
+
+name = "akshay"
+print(f"name is {name}")
+print(f"list(name) is : {list(name)}")
+print(f"[c for c in name] is : {[c for c in name]}")
+print(f"[c.upper() for c in name] is : {[c.upper() for c in name]}")
+
+
+
+first_list =[1,2,3,4,5,6,7]
+
+
+print(f"first_list is {first_list}")
+print(f" [num for num in first_list if num % 2 == 0] is { [num for num in first_list if num % 2 == 0]}")
+print(f" [num for num in first_list if num % 2 == 0] is { [num for num in first_list if num % 2 == 0]}")
+
---
+++ 08_Lists/slicing_test.py
@@ -0,0 +1,28 @@
+first_list = [1,2,3,4,5,6,7]
+
+print(f"first_list is {first_list}")
+
+print("----------------------------------------")
+print(f"first_list[1:5] is {first_list[1:5]}")
+print(f"first_list[2:] is {first_list[2:]}")
+print(f"first_list[-2:] is {first_list[-2:]}")
+print(f"first_list[20:] is {first_list[20:]}")
+print(f"first_list[:] is {first_list[:]}")
+
+print("----------------------------------------")
+print(f"first_list[:4] is {first_list[:4]}")
+print(f"first_list[:-2] is {first_list[:-2]}")
+print(f"first_list[1:-2] is {first_list[1:-2]}")
+print(f"first_list[1:4] is {first_list[1:4]}")
+
+
+print("----------------------------------------")
+
+print(f"first_list[::2] is {first_list[::2]}")
+print(f"first_list[1::-1] is {first_list[1::-1]}")
+print(f"first_list[5:2:-1] is {first_list[5:2:-1]}")
+print(f"first_list[:2:-1] is {first_list[:2:-1]}")
+print(f"first_list[:2:1] is {first_list[:2:1]}")
+
+
+print("----------------------------------------")
